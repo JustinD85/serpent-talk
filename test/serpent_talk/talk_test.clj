ead0860c583dc9813c79073d9491f7fba46685e3
Justin D <justin@baal-software.com>
2019-06-09 09:17:41 -0600

    Adds first functional test for a serpent talking

--- test/serpent_talk/talk_test.clj
+++ test/serpent_talk/talk_test.clj
@@ -5,3 +5,8 @@
 (deftest a-test
   (testing "Fixed"
     (is (= 1 1))))
+
+(deftest test-serpent-talk
+  (testing "Cries serpent! with a snake_case version of the input"
+    (is [= "Serpent! You said: hello_there"
+         (serpent-talk "hello there")])))
