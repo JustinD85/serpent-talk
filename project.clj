0517e836fbd1c5bdb563f2ea30bc03390381051e
Justin D <justin@baal-software.com>
2019-06-09 09:16:44 -0600

    Adds a main entry, add camel-snake-kebab dependency

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "git@github.com/JustinD85/serpent-talk"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns serpent-talk.talk})
+  :dependencies [
+                 [org.clojure/clojure "1.10.0"]
+                 [camel-snake-kebab "0.4.0"]]
+  :main serpent-talk.talk)
